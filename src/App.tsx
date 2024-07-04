867440c4b27c545629f17a3cdf08d02810bf379e
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2024-07-04 10:01:29 +0600

    feat: add drag overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,173 @@
-import { DndContext, DragEndEvent, DragOverEvent } from "@dnd-kit/core";
+import {
+    DndContext,
+    DragEndEvent,
+    DragOverEvent,
+    DragOverlay,
+    DragStartEvent,
+    UniqueIdentifier,
+} from "@dnd-kit/core";
 import { arrayMove } from "@dnd-kit/sortable";
 import { useState } from "react";
 import styles from "./App.module.css";
 import TaskList from "./components/task-list/TaskList";
+import TaskItem from "./components/task-item/TaskItem";
+import { restrictToWindowEdges } from "@dnd-kit/modifiers";
 
 interface ITaskList {
-  [key: string]: string[];
+    [key: string]: string[];
 }
 
 const App = () => {
-  const [taskList, setTaskList] = useState<ITaskList>({
-    Planned: ["Learn React", "Learn dnd-kit", "Learn Typescript"],
-    Processed: ["Learn CSS"],
-    Done: []
-  });
-
-  const dragEndHandler = (e: DragEndEvent) => {
-    // Check if item is drag into unknown area
-    if (!e.over || !e.active.data.current || !e.over.data.current) return;
-
-    // Check if item position is the same
-    if (e.active.id === e.over.id) return;
-
-    // Check if item is moved outside of the column
-    if (
-      e.active.data.current.sortable.containerId !==
-      e.over.data.current.sortable.containerId
-    )
-      return;
-
-    // Sort the items list order based on item target position
-    const containerName = e.active.data.current.sortable.containerId;
-    setTaskList((taskList) => {
-      const temp = { ...taskList };
-      if (!e.over) return temp;
-      const oldIdx = temp[containerName].indexOf(e.active.id.toString());
-      const newIdx = temp[containerName].indexOf(e.over.id.toString());
-      temp[containerName] = arrayMove(temp[containerName], oldIdx, newIdx);
-      return temp;
+    const [taskList, setTaskList] = useState<ITaskList>({
+        Planned: ["Learn React", "Learn dnd-kit", "Learn Typescript"],
+        Processed: ["Learn CSS", "Learn rtk query"],
+        Done: [],
+        Test: [],
     });
-  };
+    const [activeId, setActiveId] = useState<string | null | UniqueIdentifier>(
+        null
+    );
+    console.log("🚀 ~ App ~ taskList:", taskList);
 
-  const dragOverHandler = (e: DragOverEvent) => {
-    // Check if item is drag into unknown area
-    if (!e.over) return;
+    const dragStartHandler = (e: DragStartEvent) => {
+        setActiveId(e.active.id);
+    };
 
-    // Get the initial and target sortable list name
-    const initialContainer = e.active.data.current?.sortable?.containerId;
-    const targetContainer = e.over.data.current?.sortable?.containerId;
+    const dragEndHandler = (e: DragEndEvent) => {
+        // Check if item is drag into unknown area
+        setActiveId(null);
+        const { active, over } = e;
+        if (!over || !active.data.current || !over.data.current) {
+            return;
+        }
 
-    // if there are none initial sortable list name, then item is not sortable item
-    if (!initialContainer) return;
+        // Check if item position is the same
+        if (active.id === over?.id) return;
 
-    // Order the item list based on target item position
-    setTaskList((taskList) => {
-      const temp = { ...taskList };
+        // Check if item is moved outside of the column
+        if (
+            active.data.current.sortable.containerId !==
+            over.data.current.sortable.containerId
+        )
+            return;
 
-      // If there are no target container then item is moved into a droppable zone
-      // droppable = whole area of the sortable list (works when the sortable list is empty)
-      if (!targetContainer) {
-        // If item is already there then don't re-added it
-        if (taskList[e.over!.id].includes(e.active.id.toString())) return temp;
+        // Sort the items list order based on item target position
+        const containerName = active.data.current.sortable.containerId;
+        setTaskList((taskList) => {
+            const temp = { ...taskList };
+            if (!e.over) return temp;
+            const oldIdx = temp[containerName].indexOf(active.id.toString());
+            const newIdx = temp[containerName].indexOf(over.id.toString());
+            temp[containerName] = arrayMove(
+                temp[containerName],
+                oldIdx,
+                newIdx
+            );
+            return temp;
+        });
+    };
 
-        // Remove item from it's initial container
-        temp[initialContainer] = temp[initialContainer].filter(
-          (task) => task !== e.active.id.toString()
+    const dragOverHandler = (e: DragOverEvent) => {
+        // Check if item is drag into unknown area
+        const { active, over } = e;
+        if (!over) {
+            // console.log("🚀 ~ dragOverHandler ~ over:", over);
+            return;
+        }
+
+        // Get the initial and target sortable list name
+        const initialContainer = active.data.current?.sortable?.containerId;
+        const targetContainer = over?.data.current?.sortable?.containerId;
+        console.log(
+            "🚀 ~ dragOverHandler ~ initialContainer:",
+            initialContainer
         );
+        console.log("🚀 ~ dragOverHandler ~ targetContainer:", targetContainer);
 
-        // Add item to it's target container which the droppable zone belongs to
-        temp[e.over!.id].push(e.active.id.toString());
+        // if there are none initial sortable list name, then item is not sortable item
+        if (!initialContainer) return;
 
-        return temp;
-      }
+        // Order the item list based on target item position
+        setTaskList((taskList) => {
+            const temp = { ...taskList };
+            if (!targetContainer) {
+                // If item is already there then don't re-add it
+                if (taskList[over.id].includes(active.id.toString()))
+                    return temp;
 
-      // If the item is drag around in the same container then just reorder the list
-      if (initialContainer === targetContainer) {
-        const oldIdx = temp[initialContainer].indexOf(e.active.id.toString());
-        const newIdx = temp[initialContainer].indexOf(e.over!.id.toString());
-        temp[initialContainer] = arrayMove(
-          temp[initialContainer],
-          oldIdx,
-          newIdx
-        );
-      } else {
-        // If the item is drag into another different container
+                // Remove item from it's initial container
+                temp[initialContainer] = temp[initialContainer].filter(
+                    (task) => task !== active.id.toString()
+                );
 
-        // Remove item from it's initial container
-        temp[initialContainer] = temp[initialContainer].filter(
-          (task) => task !== e.active.id.toString()
-        );
+                // Add item to it's target container which the droppable zone belongs to
+                temp[over.id].push(active.id.toString());
 
-        // Add item to it's target container
-        const newIdx = temp[targetContainer].indexOf(e.over!.id.toString());
-        temp[targetContainer].splice(newIdx, 0, e.active.id.toString());
-      }
+                return temp;
+            }
 
-      return temp;
-    });
-  };
+            // If the item is drag around in the same container then just reorder the list
+            if (initialContainer === targetContainer) {
+                const oldIdx = temp[initialContainer].indexOf(
+                    active.id.toString()
+                );
+                const newIdx = temp[initialContainer].indexOf(
+                    over.id.toString()
+                );
+                temp[initialContainer] = arrayMove(
+                    temp[initialContainer],
+                    oldIdx,
+                    newIdx
+                );
+            } else {
+                // If the item is drag into another different container
 
-  return (
-    // Attach the dragEnd and dragOver event listeners
-    <DndContext onDragEnd={dragEndHandler} onDragOver={dragOverHandler}>
-      <main className={styles.main}>
-        <h1>Multi Sortable List</h1>
-        <section className={styles.container}>
-          {Object.keys(taskList).map((key) => (
-            <TaskList key={key} title={key} tasks={taskList[key]} />
-          ))}
-        </section>
-      </main>
-    </DndContext>
-  );
+                // Remove item from it's initial container
+                temp[initialContainer] = temp[initialContainer].filter(
+                    (task) => task !== active.id.toString()
+                );
+
+                // Add item to it's target container
+                const newIdx = temp[targetContainer].indexOf(
+                    over.id.toString()
+                );
+                temp[targetContainer].splice(newIdx, 0, active.id.toString());
+            }
+
+            return temp;
+        });
+    };
+
+    return (
+        // Attach the dragEnd and dragOver event listeners
+        <DndContext
+            onDragEnd={dragEndHandler}
+            onDragOver={dragOverHandler}
+            onDragStart={dragStartHandler}
+            modifiers={[restrictToWindowEdges]}
+        >
+            <main className="flex flex-col items-center gap-8 py-8 px-16">
+                <h1>Multi Sortable List</h1>
+                {/* <div>
+                    <TaskList activeId={activeId}></TaskList>
+                </div> */}
+                <div className={styles.container}>
+                    {/* <div className="grid grid-cols-[repeat(2,1fr)] justify-evenly w-[75%] p-4 border border-black"> */}
+                    {Object.keys(taskList).map((key) => (
+                        <TaskList
+                            activeId={activeId}
+                            key={key}
+                            title={key}
+                            tasks={taskList[key]}
+                        />
+                    ))}
+                </div>
+            </main>
+            <DragOverlay>
+                {activeId ? <TaskItem title={activeId} dragOverlay /> : null}
+            </DragOverlay>
+        </DndContext>
+    );
 };
 
 export default App;
--- src/components/task-list/TaskList.tsx
+++ src/components/task-list/TaskList.tsx
@@ -1,30 +1,40 @@
-import { useDroppable } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { DragOverlay, UniqueIdentifier, useDroppable } from "@dnd-kit/core";
+import {
+    SortableContext,
+    verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import { FC } from "react";
 import TaskItem from "../task-item/TaskItem";
 import styles from "./TaskList.module.css";
 
 interface ITaskList {
-  title: string;
-  tasks: string[];
+    title: string;
+    tasks: string[];
+    activeId?: UniqueIdentifier | null;
 }
 
 const TaskList: FC<ITaskList> = (props) => {
-  const { setNodeRef } = useDroppable({ id: props.title });
+    const { setNodeRef } = useDroppable({ id: props.title });
 
-  return (
-    <article className={styles.column}>
-      <h1>{props.title}</h1>
-      <div className={styles.divider} />
-      <SortableContext id={props.title} items={props.tasks}>
-        <ul ref={setNodeRef} className={styles.list}>
-          {props.tasks.map((task) => (
-            <TaskItem key={task} title={task} />
-          ))}
-        </ul>
-      </SortableContext>
-    </article>
-  );
+    return (
+        // <article className="flex flex-col items-center p-4 border border-black min-h-[100%] w-[30%] gap-4">
+        <article className={styles.column}>
+            <h1 className="text-lg">{props.title}</h1>
+            <div className={styles.divider} />
+            {/* <div className={"border border-black w-full"} /> */}
+            <SortableContext id={props.title} items={props.tasks}>
+                {/* <ul
+          ref={setNodeRef}
+          className="flex flex-col gap-4 w-full h-full bg-[beige]"
+        > */}
+                <ul ref={setNodeRef} className={styles.list}>
+                    {props.tasks.map((task) => (
+                        <TaskItem key={task} title={task} />
+                    ))}
+                </ul>
+            </SortableContext>
+        </article>
+    );
 };
 
 export default TaskList;
